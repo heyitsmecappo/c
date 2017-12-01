8d2836cc78653903a2898a6fbd6e5b8eec87bf22
heyitsmecappo <33719359+heyitsmecappo@users.noreply.github.com>
2017-12-01 12:55:03 +0100

    Add files via upload

---
+++ suma matrices.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int matriz[2][2];
+    int otramatriz[2][2];
+    int matrizsuma[2][2]; 
+    int salir;
+    int fila,col;
+    for(fila=0;fila<2;fila++){
+                              for(col=0;col<3;col++){
+                                                     cout<<"matriz["<<fila<<"]["<<col<<"]";
+                                                     cin>>matriz[fila][col];
+ 
