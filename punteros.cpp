0e91e505692cb00a552ca70f2da5b89dde36c924
heyitsmecappo <33719359+heyitsmecappo@users.noreply.github.com>
2018-01-26 12:50:23 +0100

    Add files via upload

---
+++ punteros.cpp
@@ -0,0 +1,18 @@
+//Programas sobre manejo de memoria
+#include<stdio.h>
+int main(){
+    char palabra1[5];
+    char palabra2[5];
+    int salir;
+    printf("Dime algo: ");
+    scanf("%s",palabra1);
+    //%s para guardar una cadena
+    //%x para numeros hexadecimales
+    printf("\nDime algo mas: ");
+    scanf("%s",palabra2);
+    printf("direccion palabra1 = %x",palabra1);
+    printf("palabra1 = %c\n",*(palabra1+6));
+    printf("direccion palabra2 = %x",palabra2);
+    printf("palabra2 = %c  ",*(palabra2+7));
+    scanf("%i",&salir);
+}   
