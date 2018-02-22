f10f2452943a700dfd835bccfdfef4247c3662b3
heyitsmecappo <33719359+heyitsmecappo@users.noreply.github.com>
2018-02-22 11:50:50 +0100

    Add files via upload

---
+++ agendatlf.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    struct datos{
+           string nombre;
+           int telefono;
+           string email;
+           int dia;
+           int mes;
+           int ano;
+ };
+ int y;
+ int x;
+ int z;
+ int salir;
+ //voy a hacerlo bilingue
+ cout<<"English - 1    Espanol - 2:    "<<endl;
+ cin>>z;
+        if(z=2){{
+            cout<<" ==>  Cuantos amigos tienes?:    "<<endl;
+            cin>>y;
+            datos amigo[y];
+
+            for(x=0;x<y;x++){
+                  cout<<" ==> Datos amigo numero "<<x+1<<" :"<<endl; 
+                  cout<<"    ==> Nombre:   ";
+                  cin>>amigo[x].nombre;
+                  cout<<"    ==> Telefono:     ";
+                  cin>>amigo[x].telefono;
+                  cout<<"    ==> E-mail:   ";
+                  cin>>amigo[x].email;
+                  cout<<"    ==> Dia de nacimiento:   ";
+                  cin>>amigo[x].dia;
+                  cout<<"    ==> Mes de nacimiento:   ";
+                  cin>>amigo[x].mes;
+                  cout<<"    ==> Ano de nacimiento:   ";
+                  cin>>amigo[x].ano;
+                  }                       
+//quiero guardar la agenda en un fichero            
+                 ofstream file("Agenda de telefono.txt");
+                   for(x=0;x<y;x++){
+                          file<<" Datos amigo"<<x+1<<endl;                                              
+                          file<<"  - Nombre:   "<<amigo[x].nombre<<endl;
+                          file<<"  - Telefono:     "<<amigo[x].telefono<<endl;
+                          file<<"  - E-mail:   "<<amigo[x].email<<endl;
+                          file<<"  - Cumpleaños:   "<<amigo[x].dia<<"/"<<amigo[x].mes<<"/"<<amigo[x].ano<<endl;
+                          }
+                          file.close();
+
+return 0;
+}
+       if(z=1){ 
+                cout<<"How many friends do you have?:    "<<endl;
+                cin>>y;
+                datos amigo[y];
+
+                for(x=0;x<y;x++){
+                  cout<<" ==> Friend number "<<x+1<<" info: "<<endl; 
+                  cout<<"    ==> Name:   ";
+                  cin>>amigo[x].nombre;
+                  cout<<"    ==> Phone:     ";
+                  cin>>amigo[x].telefono;
+                  cout<<"    ==> E-mail:   ";
+                  cin>>amigo[x].email;
+                  cout<<"    ==> Birthday day:   ";
+                  cin>>amigo[x].dia;
+                  cout<<"    ==> Birthday month:   ";
+                  cin>>amigo[x].mes;
+                  cout<<"    ==> Birthday year:   ";
+                  cin>>amigo[x].ano;
+                  }                                   
+         ofstream file("List of contacts.txt");
+                  for(x=0;x<y;x++){
+                     file<<" Friend "<<x+1<<" info: "<<endl;                                              
+                     file<<" - Name:   "<<amigo[x].nombre<<endl;
+                     file<<" - Phone:     "<<amigo[x].telefono<<endl;
+                     file<<" - E-mail:   "<<amigo[x].email<<endl;
+                     file<<" - Birthday date:   "<<amigo[x].dia<<"/"<<amigo[x].mes<<"/"<<amigo[x].ano<<endl;
+}   
+                     file.close();
+
+}}
+return 0;
+}
